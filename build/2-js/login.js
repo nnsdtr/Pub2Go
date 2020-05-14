620c4f96f52e23e44210e842768f47f92dfeb18d
Daniel T Nunes <nnsdtr.github@gmail.com>
2020-05-14 02:31:49 -0300

    inclusão de funcionalidade JavaScript na página de login e associação do link para cadastro do cliente

---
+++ build/2-js/login.js
@@ -0,0 +1,68 @@
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+};
+
+function validateForm() {
+    if( !validateEmail(email.value) || senha.value.length == 0 ) {
+        btnSubmit.disabled = true;
+        btnSubmit.style.background = '#d4cec6';
+        btnSubmit.style.color = '#9b9797';
+        btnSubmit.style.border = '2px solid #d4cec6';
+        btnSubmit.style.fontWeight = 'normal';
+    } else {
+        btnSubmit.disabled = false;
+        btnSubmit.style.background = '';
+        btnSubmit.style.color = '';
+        btnSubmit.style.border = '';
+        btnSubmit.style.fontWeight = '';
+    };
+};
+
+window.onload = () => {
+
+    email.oninput = () => {
+
+        validateForm();
+
+        if( !validateEmail(email.value) ) {
+            instrucaoEmail.innerHTML = '* Verifique a digitação do email';
+            instrucaoEmail.style.color = '#D01';
+            instrucaoEmail.style.display = 'block';
+            instrucaoEmail.style.textAlign = 'left';
+            email.style.background = '#fcfaf9';
+
+        } else {
+            instrucaoEmail.innerHTML = '';
+            email.style.background = 'white';
+        };
+    };
+
+    senha.oninput = () => {
+        validateForm();
+
+        if( senha.value.length > 0 ) {
+            instrucaoSenha.innerHTML = '';
+            senha.style.background = 'white';
+        } else {
+            instrucaoSenha.innerHTML = '* Este campo deve ser preenchido';
+            instrucaoSenha.style.color = '#D01';
+            instrucaoSenha.style.display = 'block';
+            instrucaoSenha.style.textAlign = 'left';
+            senha.style.background = '#fcfaf9';
+        };
+    };
+
+    loginForm.onsubmit = () => {
+        let user = {email: email.value, senha: senha.value};
+        localStorage.setItem('user', JSON.stringify(user));
+    };
+
+    if(localStorage.getItem('user')){
+        user = JSON.parse(localStorage.getItem('user'));
+        email.value = user.email;
+    };
+};
+
+
+
