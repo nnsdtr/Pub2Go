bb6c22cc517f064a06de024becc1c875738c0ae2
Octávio Martins <octaviomartins10@gmail.com>
2020-05-21 13:05:03 -0300

    Alterações Páginas

--- build/2-js/perfil-cliente.js
+++ build/2-js/perfil-cliente.js
@@ -1,4 +1,18 @@
-$('#clicavel').click(function()
-						{
-							console.log("Clicked");
-                        });
+
+/* Aparecer o nome da imagem selecionada*/
+jQuery(function ($) {
+	$('input[type="file"]').change(function () {
+		if ($(this).val()) {
+			var filename = $(this).val();
+			$(this).closest('.file-upload').find('.file-name').html(filename);
+		}
+	});
+});
+
+/**/
+
+$('#clicavel').click(function () {
+	console.log("Clicked");
+});
+
+/**/
