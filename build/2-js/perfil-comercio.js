9e45f257e1fb8fe02e9337439bd8059d360acfb7
Maycon Felipe <felipemaycon26@gmail.com>
2020-07-06 12:17:53 -0300

    Perfil do comércio com localstorage

--- build/2-js/perfil-comercio.js
+++ build/2-js/perfil-comercio.js
@@ -1,9 +1,19 @@
-$(".star.glyphicon").click(function() {
-    $(this).toggleClass("glyphicon-star glyphicon-star-empty");
-  });
-  
-  $(".heart.fa").click(function() {
-    $(this).toggleClass("fa-heart fa-heart-o");
-  });
-  
-  
+onload = () => {
+
+	/* Pegar a referencia do array usuarios no db */
+	let db_bares = JSON.parse(localStorage.getItem('db_bares'));
+
+  let nome = db_bares.data["0"].nome;
+  let endereco = db_bares.data["0"].logradouro + ", " + db_bares.data["0"].numeroLocal + " - " + db_bares.data["0"].bairro + ", " + db_bares.data["0"].cidade + " - " + db_bares.data["0"].estado + ", " + db_bares.data["0"].cep;
+  let telefone = db_bares.data["0"].telefone;
+
+
+  let nomeBar = document.getElementById('nome');
+  let enderecoBar = document.getElementById('endereco');
+  let telefoneBar = document.getElementById('telefone');
+
+  nomeBar.innerText = nome;
+  enderecoBar.innerText = endereco;
+  telefoneBar.innerText = telefone;
+
+}
